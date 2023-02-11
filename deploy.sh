5e9cab327a9ecaa348ca366a03a2a05ddf5d2349
Leandro Lima dos Santos <leandrolsadm@gmail.com>
2023-02-11 11:50:26 -0300

    updates

--- deploy.sh
+++ deploy.sh
@@ -16,10 +16,10 @@ git init
 git add -A
 git commit -m 'deploy'
 
-# if you are deploying to https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
+# if you are deploying to https://leeoliima.github.io
+# git push -f git@github.com:leeoliima/leeoliima.github.io.git main
 
-# if you are deploying to https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
+# if you are deploying to https://leeoliima.github.io/<REPO>
+# git push -f git@github.com:leeoliima/<REPO>.git main:gh-pages
 
 cd -
